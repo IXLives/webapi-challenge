0acbc2bf106a7d46820a73c5b038cd3360ce0947
Ian Bryant <48700036+IXLives@users.noreply.github.com>
2019-08-17 13:03:20 -0400

    Project router done up good

--- projects/project-router.js
+++ projects/project-router.js
@@ -33,78 +33,116 @@ router.get("/:id", validateProjectId, async (req, res) => {
 
 //Get Project Actions
 router.get("/:id/actions", validateProjectId, async (req, res) => {
-    try {
-        const actions = await db.getProjectActions(req.params.id)
+  try {
+    const actions = await db.getProjectActions(req.params.id);
 
-        if(actions.length < 1) {
-            res.status(300).json({
-                message:"The project has no actions"
-            })
-        } else {
-            res.status(201).json(actions)
-        }
-    } catch(error) {
-        res.status(500).json({
-            message: "The project's actions could not be retrieved"
-        })
+    if (actions.length < 1) {
+      res.status(300).json({
+        message: "The project has no actions"
+      });
+    } else {
+      res.status(201).json(actions);
     }
-})
+  } catch (error) {
+    res.status(500).json({
+      message: "The project's actions could not be retrieved"
+    });
+  }
+});
 
 //Add a project
 router.post("/", validateProjectBody, async (req, res) => {
-    try {
-        const project = req.body
+  try {
+    const project = req.body;
 
-        if (project.name && project.description) {
-            await db.insert(project)
-            res.status(201).json(project)
-        } else {
-            res.status(400).json({
-                message: "New projects require a Name and Description"
-            })
-        }
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({
-            message: "There was an error while adding the Project to the database"
-        })
+    if (project.name && project.description) {
+      await db.insert(project);
+      res.status(201).json(project);
+    } else {
+      res.status(400).json({
+        message: "New projects require a Name and Description"
+      });
     }
-})
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "There was an error while adding the Project to the database"
+    });
+  }
+});
 
 //Edit a project
+router.put(
+  "/:id",
+  [validateProjectId, validateProjectBody],
+  async (req, res) => {
+    try {
+      const id = req.params.id;
+      const changes = req.body;
+
+      if (changes.name && changes.description) {
+        await db.update(id, changes);
+        res.status(201).json(changes);
+      } else {
+        res.status(400).json({
+          message: "New projects require a Name and Description"
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        message: "There was an error while adding the Project to the database"
+      });
+    }
+  }
+);
 
 //Delete a project
+router.delete("/:id", validateProjectId, async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    await db.remove(id);
+    res.status(201).json({
+      message: "Deleted"
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "There was a problem deleting the project"
+    });
+  }
+});
 
 //Middlewarez
-
 //Check if project id exists
 function validateProjectId(req, res, next) {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    db.get(id)
-      .then(project => {
-        if (project) {
-          next();
-        } else {
-          res.status(404).json({ message: "No project with given id." });
-        }
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json({
-          message: "Error processing request."
-        });
+  db.get(id)
+    .then(project => {
+      if (project) {
+        next();
+      } else {
+        res.status(404).json({ message: "No project with given id." });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        message: "Error processing request."
       });
+    });
 }
 
 //Check post for valid body
 function validateProjectBody(req, res, next) {
-    if (req.body && Object.keys(req.body).length > 0) {
-      next();
-    } else {
-      next({ message: "Bad request." });
-    }
+  if (req.body && Object.keys(req.body).length > 0) {
+    next();
+  } else {
+    next({ message: "Bad request." });
   }
+}
 
 //Export
 module.exports = router;
